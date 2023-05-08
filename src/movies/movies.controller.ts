87bba2e695addbb6dc5c6b3226d6b3510d2e170a
dev-hana <jieunj53@gmail.com>
2023-05-08 22:23:21 +0900

    Movies Service part One

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,41 +1,52 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
 
-@Controller('movies')   // router: /movies
+@Controller('movies') // router: /movies
 export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
 
-    @Get()
-    getAll(){
-        return "This will return all movies"
-    }
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
 
-    @Get("search")
-    search(@Query('year') searchingYear: string){
-        return `We are searching for a movie made after: ${searchingYear} `
-    }
-    // 아래 Get보다 위에 있어야 /search 작동
+  // @Get('search')
+  // search(@Query('year') searchingYear: string) {
+  //   return `We are searching for a movie made after: ${searchingYear} `;
+  // }
+  // 아래 Get보다 위에 있어야 /search 작동
 
-    @Get("/:id") // If you want somethig, you have to ask for it.
-    getOne(@Param("id") movieId:string){
-        return `Thie will return one movie with the id : ${movieId}`
-    }
+  @Get('/:id') // If you want somethig, you have to ask for it.
+  getOne(@Param('id') movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
+  }
 
-    @Post()
-    crate(@Body() movieData){
-        // console.log(movieData)
-        return movieData
-    }
+  @Post()
+  crate(@Body() movieData) {
+    // console.log(movieData)
+    return this.moviesService.create(movieData);
+  }
 
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return this.moviesService.deleteOne(movieId);
+  }
 
-    @Delete("/:id")
-    remove(@Param("id") movieId: string){
-        return `This will remove a movie with the id ${movieId}`
-    }
-
-    @Patch('/:id')
-    patch(@Param("id") movieId: string, @Body() updateData){
-        return {
-            updateMovie: movieId,
-            ...updateData
-        }
-    }
+  @Patch('/:id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updateMovie: movieId,
+      ...updateData,
+    };
+  }
 }
