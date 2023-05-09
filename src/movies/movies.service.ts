e13f6e3ab01ff319b59a38efb831434aba55cf6d
dev-hana <jieunj53@gmail.com>
2023-05-09 21:43:11 +0900

    Express on NestJS

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, Req, Res } from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
 import { CreateMovieDTO } from './dto/create-movie.dto';
 import { UpdateMovieDTO } from './dto/update-movie.dto';
@@ -7,6 +7,11 @@ import { UpdateMovieDTO } from './dto/update-movie.dto';
 export class MoviesService {
   private movies: Movie[] = [];
 
+  // @Req() req, @Res() res => Express앱에 접근 가능
+  // NestJS가 Express 위에서 돌아가기 때문(Fastify로 전환 가능, 두개의 프레임워크 위에서 동시에 돌아감)
+  // 따라서 Express 객체를 직접적으로 사용하는 것은 좋은 방법이 아님
+  // NestJS 방식만 사용하면 Express에서 Fastify로 전환할 때 멈추지 않음
+  // getAll(@Req() req, @Res() res): Movie[] {
   getAll(): Movie[] {
     return this.movies;
   }
